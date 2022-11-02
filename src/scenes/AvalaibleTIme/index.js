03a3f7e019121ce579e754afd7741e4d95090bbe
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-11-02 10:24:06 +0530

    "avalaible time update"

--- src/scenes/AvalaibleTIme/index.js
+++ src/scenes/AvalaibleTIme/index.js
@@ -1,5 +1,5 @@
 import { Box} from "@mui/system";
- import {Button,Typography}   from "@mui/material";
+ import {Button,Grid,Typography}   from "@mui/material";
 import React from 'react'
 import Header from '../../components/Header';
 import AddLocationTwoToneIcon from "@mui/icons-material/AddLocationTwoTone";
@@ -17,6 +17,18 @@ const AvalaibleTime = () => {
           title="Avalaible Time"
           subtitle="Find your suitable TimeSlots  ..."
         />
+        <Box mb="34px">
+          <Typography
+            variant="h4"
+            color={colors.greenAccent[500]}
+            fontWeight="bold"
+            sx={{ m: "0 0 5px 0" }}
+            textAlign="center"
+          >
+            Appoinment Details
+          </Typography>
+        </Box>
+
         <Box m="20px" display="flex" justifyContent="space-around">
           <Box
             display="flex"
@@ -33,18 +45,18 @@ const AvalaibleTime = () => {
               <Box display="flex" justifyContent="space-around">
                 <Box>
                   <Typography
-                    variant="h4"
+                    variant="h5"
                     color={colors.grey[100]}
                     fontWeight="bold"
                     textAlign="center"
                     sx={{ m: "0 0 5px 0" }}
                   >
-                    TDM ID :TDMId01
+                    TDM ID : TDMId01
                   </Typography>
                 </Box>
                 <Box>
                   <Typography
-                    variant="h4"
+                    variant="h5"
                     color={colors.grey[100]}
                     fontWeight="bold"
                     textAlign="center"
@@ -57,7 +69,7 @@ const AvalaibleTime = () => {
               <Box display="flex" justifyContent="space-around">
                 <Box>
                   <Typography
-                    variant="h4"
+                    variant="h5"
                     color={colors.grey[100]}
                     fontWeight="bold"
                     textAlign="center"
@@ -68,13 +80,13 @@ const AvalaibleTime = () => {
                 </Box>
                 <Box>
                   <Typography
-                    variant="h4"
+                    variant="h5"
                     color={colors.grey[100]}
                     fontWeight="bold"
                     textAlign="center"
                     sx={{ m: "0 0 5px 0" }}
                   >
-                    Doctor Id : DocId01
+                    Doctor ID : DocId01
                   </Typography>
                 </Box>
               </Box>
@@ -87,11 +99,11 @@ const AvalaibleTime = () => {
               />
             </Box>
           </Box>
-          <Box>
+          <Box display="flex" justifyContent="center" alignItems="center">
             <Button
-              color="secondary"
+               color="secondary"
               variant="contained"
-              sx={{ width: 120, height: 72 }}
+              sx={{ width: "5rem", height: "3rem" }}
               startIcon={<VerifiedIcon color={colors.grey[100]} />}
             >
               Verify
@@ -99,38 +111,334 @@ const AvalaibleTime = () => {
           </Box>
         </Box>
         <Box m="30px 0 0 0" height="60vh">
-          HEY
+          <Box mb="34px">
+            <Typography
+              variant="h4"
+              color={colors.greenAccent[500]}
+              fontWeight="bold"
+              sx={{ m: "0 0 5px 0" }}
+              textAlign="center"
+            >
+              Book your Time slot
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="space-between" m="24px">
+            <Box
+              sx={{
+                height: "55vh",
+
+                minWidth: "35vw",
+                border: 1,
+                borderRadius: "7px",
+              }}
+            >
+              <Box>
+                <Typography
+                  variant="h4"
+                  fontWeight="bold"
+                  sx={{ m: "0 0 5px 0" }}
+                  textAlign="center"
+                >
+                  Morning-AM
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  display: "grid",
+                  gridTemplateColumns: "repeat(3, 1fr)",
+                  rowGap: 2,
+                  columnGap: 3,
+                  width: "100%",
+                  height: "75%",
+                  marginTop: 4,
+                }}
+              >
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                    disabled
+                  >
+                    6.15
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    7.15
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    8.00
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    6.30
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                    disabled
+                  >
+                    7.30
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    8.15
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    6.45
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    7.45
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    8.30
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    8.45
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    9.00
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    9.30
+                  </Button>
+                </Box>
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                height: "55vh",
+                minWidth: "35vw",
+                border: 1,
+                borderRadius: "7px",
+              }}
+            >
+              <Box>
+                <Typography
+                  variant="h4"
+                  fontWeight="bold"
+                  sx={{ m: "0 0 5px 0" }}
+                  textAlign="center"
+                >
+                  Evening-PM
+                </Typography>
+              </Box>
+              <Grid
+                sx={{
+                  display: "grid",
+                  gridTemplateColumns: "repeat(3, 1fr)",
+                  rowGap: 2,
+                  columnGap: 3,
+                  width: "100%",
+                  height: "75%",
+                  marginTop: 4,
+                }}
+              >
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    6.15
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    7.15
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                    disabled
+                  >
+                    8.00
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    6.30
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                    disabled
+                  >
+                    7.30
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    8.15
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    6.45
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    7.45
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    8.30
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    8.45
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    9.00
+                  </Button>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    color="success"
+                    variant="contained"
+                    sx={{ fontSize: "1rem", color: "white" }}
+                  >
+                    9.15{" "}
+                  </Button>
+                </Box>
+              </Grid>
+            </Box>
+          </Box>
         </Box>
-        <Box m="20px 0 0 0" display="flex" justifyContent="space-between">
-          <Box>
-            <Button
-              color="secondary"
-              variant="contained"
-              sx={{ width: 50, height: 30 }}
-            >
-              Back
-            </Button>
-          </Box>
-          <Box
-            display="flex"
-            sx={{ width: "15%" }}
-            justifyContent="space-between"
+      </Box>
+      <Box m="20px 0 0 0" display="flex" justifyContent="space-between">
+        <Box display="flex" alignItems="center">
+          <Button
+            color="secondary"
+            variant="contained"
+            sx={{ width: 50, height: 30 }}
           >
-            <Button
-              color="secondary"
-              variant="contained"
-              sx={{ width: 50, height: 30 }}
-            >
-              Refresh
-            </Button>
-            <Button
-              color="secondary"
-              variant="contained"
-              sx={{ width: 50, height: 30 }}
-            >
-              Submit
-            </Button>
-          </Box>
+            Back
+          </Button>
+        </Box>
+        <Box
+          display="flex"
+          sx={{ width: "15%" }}
+          margin="18px"
+          justifyContent="space-between"
+        >
+          <Button
+            color="secondary"
+            variant="contained"
+            sx={{ width: 50, height: 30 }}
+          >
+            Refresh
+          </Button>
+          <Button
+            color="secondary"
+            variant="contained"
+            sx={{ width: 50, height: 30 }}
+          >
+            Submit
+          </Button>
         </Box>
       </Box>
     </>
